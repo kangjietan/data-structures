88be716fc9d6ba59afcc9d4809caed3e6f853852
Hack Reactor Student <student@hackreactor.com>
2019-12-13 15:25:26 -0800

    BinarySearchTree completed

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,5 +1,76 @@
-var BinarySearchTree = function(value) {
+var BinarySearchTree = function (value) {
+  var newTree = {};
+  _.extend(newTree, binaryTreeMethods);
+  newTree.value = value;
+  newTree.left = null;
+  newTree.right = null;
+  newTree.children = 0;
+
+  return newTree;
 };
+
+var binaryTreeMethods = {};
+
+binaryTreeMethods.insert = function (value) {
+
+  var recursion = function (x) {
+    if (x.value > value) {
+      if (x.left === null) {
+        x.left = BinarySearchTree(value);
+      } else {
+        recursion(x.left);
+      }
+    } else if (x.value < value) {
+      if (x.right === null) {
+        x.right = BinarySearchTree(value);
+      } else {
+        recursion(x.right);
+      }
+    }
+  };
+
+  recursion(this);
+};
+
+binaryTreeMethods.contains = function (value) {
+  var result = false;
+
+  var recursion = function(x) {
+    if (x.value === value) {
+      result = true;
+    }
+
+    if (value > x.value) {
+      if (x.right !== null) {
+        recursion(x.right);
+      }
+    } else if (value < x.value) {
+      if (x.left !== null) {
+        recursion(x.left);
+      }
+    }
+  };
+
+  recursion(this);
+
+  return result;
+};
+
+binaryTreeMethods.depthFirstLog = function (callback) {
+  var recursion = function (x) {
+    callback(x.value);
+    if (x.left) {
+      recursion(x.left);
+    }
+    if (x.right) {
+      recursion(x.right);
+    }
+  };
+
+  recursion(this);
+};
+
+
 
 
 /*
